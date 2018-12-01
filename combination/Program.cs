fb8eb1fc0de5502a91bebbc672a0a901e438d9bf
Александр Фомин <neiasit@bk.ru>
2018-12-01 12:13:28 +0900

    done

--- combination/Program.cs
+++ combination/Program.cs
@@ -8,39 +8,22 @@ namespace combination
 {
     class Program
     {
-        static int Factorial(int x)
+        static int[][] Combination(params int[][] numbersIn)
         {
-            int result = 1;
-            for (int i = 1; i <= x; i++)
-            {
-                result *= i;
-            }
-            return result;
-        }
-
-        static void Main(string[] args)
-        {
-
-            int[][] numbersIn = new int[4][];
-            numbersIn[0] = new int[] { 1, 2 };
-            numbersIn[1] = new int[] { 3, 4, 13, 14, 66 };
-            numbersIn[2] = new int[] { 5, 6, 7, 8, 9 };
-            numbersIn[3] = new int[] { 10, 11, 12};
-
             //находим общее число комбинаций
-            int NumbersOfComb = 1;
+            int numbersOfComb = 1;
             for (int l = 0; l < numbersIn.Length; l++)
             {
-                NumbersOfComb *= numbersIn[l].Length;
+                numbersOfComb *= numbersIn[l].Length;
             }
             //создаем выходной массив массивов
-            int[][] numbersOut = new int[NumbersOfComb][];
-            for(int l = 0; l < NumbersOfComb; l++)
-            { 
+            int[][] numbersOut = new int[numbersOfComb][];
+            for (int l = 0; l < numbersOfComb; l++)
+            {
                 numbersOut[l] = new int[numbersIn.Length];
             }
-            //заполняем выходной массив
 
+            //заполняем выходной массив
             int h = 0;// элемент входного массива
             int j = 0;// элемнет выходного массива
             int i = 0;// индекс выходного массива
@@ -48,25 +31,24 @@ namespace combination
             int k = 0; // количество периодов использования
             for (; i < numbersOut.Length && j < numbersOut[0].Length;)
             {
-                if (i==0)
+                if (i == 0)
                 {
                     h = 0;
-                    for (int l = j+1; l < numbersIn.Length; l++)
+                    for (int l = j + 1; l < numbersIn.Length; l++)
                     {
-                         period *= numbersIn[l].Length;                        
-                    }
-                    Console.WriteLine(period);
-                }                
+                        period *= numbersIn[l].Length;
+                    }                   
+                }
                 if (k == period)
                 {
                     h++;
                     if (h == numbersIn[j].Length)
                     {
-                        h=0;
+                        h = 0;
                     }
                     k = 0;
                 }
-                Console.WriteLine($"{i} {j} {j} {h}");
+ 
                 numbersOut[i][j] = numbersIn[j][h];
                 i++;
                 k++;
@@ -75,9 +57,22 @@ namespace combination
                     i = 0;
                     j++;
                     period = 1;
-                    k=0;
+                    k = 0;
                 }
             }
+
+            return numbersOut;
+        }
+
+        static void Main(string[] args)
+        {            
+            int[] numbersIn0 = new int[] { 1, 2 };
+            int[] numbersIn1 = new int[] { 3, 4, 13, 14, 66 };
+            int[] numbersIn2 = new int[] { 5, 6, 7, 8, 9 };
+            int[] numbersIn3 = new int[] { 10, 11, 12 };
+
+            int[][] numbersOut = Combination(numbersIn0, numbersIn1, numbersIn2, numbersIn3);
+
             for (int l = 0; l < numbersOut.Length; l++)
             {
                 Console.Write($"{l + 1}\t");
